13b4647a29598b6385444614a83c9604f7c20260
arrrmann <arman.arustamyan99@gmail.com>
2023-09-22 18:43:34 +0400

    fix

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,115 +1,128 @@
 import { Component } from "react";
+import { FaCheck } from "react-icons/fa6";
 import './register-page.css'
 
 
 export default class RegisterPage extends Component {
-    state = {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    validationErrors: {},
+    image: null,
+  }
+
+  validateEmail = (email) => {
+    const shnik = email.indexOf("@")
+    const ket = email.lastIndexOf(".")
+    if (shnik < 1 || ket < shnik + 2 || ket + 2 >= email.length) {
+      return false
+    } else {
+      return true
+    }
+  }
+  validatePassword = (password) => {
+    if (password.length <= 6) return false
+
+    let hasUperCaseLetter = false
+    let hasNumbers = false
+
+    for (let i = 0; i < password.length; i++) {
+      const char = password[i]
+
+      if (char >= 'A' && char <= 'Z') hasUperCaseLetter = true
+      if (char >= '0' && char <= '9') hasNumbers = true
+    }
+
+    return (hasNumbers && hasUperCaseLetter)
+  }
+
+  onInputChange = (e) => {
+    const { name, value } = e.target
+    this.setState((prevState) => {
+      return {
+        [name]: value
+      }
+    })
+  }
+
+  handleRegister = () => {
+
+    const { name, email, password, image } = this.state;
+    const validationErrors = {}
+
+    if (name.trim().length < 3) {
+      validationErrors.name = 'Username is required.'
+    }
+    if (!email.trim() || !this.validateEmail(email)) {
+      validationErrors.email = 'Please enter a valid email.'
+    }
+    if (!password.trim() || !this.validatePassword(password)) {
+      validationErrors.password = 'Password must have more than 6 charecters. It must includes capitale letters and digits'
+    }
+
+    if (Object.keys(validationErrors).length === 0) {
+      this.props.handleData({ name, email, password, image })
+      this.setState({
         name: '',
         email: '',
         password: '',
         validationErrors: {},
-        image: undefined,
+        image: undefined
+      })
+    } else {
+      this.setState({ validationErrors })
     }
+  }
 
-    validateEmail = (email) => {
-        const shnik = email.indexOf("@")
-        const ket = email.lastIndexOf(".")
-        if (shnik < 1 || ket < shnik + 2 || ket + 2 >= email.length) {
-            return false
-        } else {
-            return true
-        }
+
+  onImageChange = (event) => {
+    const file = event.target.files[0]
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const imgSRC = e.target.result
+      this.setState({
+        image: imgSRC
+      })
     }
-    validatePassword = (password) => {
-        if(password.length <= 6) return false
-        
-        let hasUperCaseLetter=false
-        let hasNumbers=false
+    reader.readAsDataURL(file)
+  }
 
-        for(let i=0; i<password.length; i++){
-            const char = password[i]
+  render() {
+    const { name, email, password, validationErrors, image } = this.state
 
-            if(char>='A' && char<='Z') hasUperCaseLetter=true
-            if (char>='0' && char<='9') hasNumbers=true
-        }
-
-        return hasNumbers && hasUperCaseLetter
-    }
-
-    onInputChange = (e) => {
-        const { name, value } = e.target
-        this.setState((prevState) => {
-            return {
-                [name]: value
-            }
-        })
-    }
-
-    handleRegister = () => {
-
-        const { name, email, password, image } = this.state;
-        const validationErrors = {}
-
-        if (!email.trim() || !this.validateEmail(email)) {
-            validationErrors.email = 'Please enter a valid email.'
-        }
-        if (!password.trim() || !this.validatePassword(password)) {
-            validationErrors.password = 'Password must have more than 6 charecters. It must includes capitale letters and digits'
-        }
-        if (name.trim().length < 3) {
-            validationErrors.name = 'Username is required.'
-        }
-        if (Object.keys(validationErrors).length === 0) {
-            this.props.handleData({ name, email, password, image })
-            this.setState({
-                name: '',
-                email: '',
-                password: '',
-                validationErrors: {},
-                image: undefined
-            })
-        } else {
-
-            this.setState({ validationErrors })
-        }
-    }
-
-
-    onImageChange = (event) => {
-        const file = event.target.files[0]
-        const reader = new FileReader()
-        reader.onload = (e) => {
-            const imgSRC = e.target.result
-            this.setState({
-                image: imgSRC
-            })
-        }
-        reader.readAsDataURL(file)
-    }
-
-    render() {
-        const { name, email, password, validationErrors } = this.state
-
-        return (
-            <div className="container">
-                <h1 className="sign">Sign up</h1>
-                <div className="wrapper">
-                    <input type="file" accept="image/*" onChange={this.onImageChange} />
-                    <input name='name' value={name} onChange={this.onInputChange} placeholder="Enter username" />
-                    <input name='email' value={email} onChange={this.onInputChange} placeholder="Enter e-mail" />
-                    <input name='password' value={password} onChange={this.onInputChange} placeholder="Enter password" />
-                    {
-                        Object.keys(validationErrors).length ? (
-                            <div className="error-alert">
-                                <span>{validationErrors.email}</span>
-                                <span>{validationErrors.password}</span>
-                                <span>{validationErrors.name}</span>
-                            </div>
-                        ) : null
-                    }
-                    <button onClick={this.handleRegister} className="btn">Sign up</button>
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="register-page-wrapper">
+        <h1 className="sign">Sign up</h1>
+        <div className="register-form">
+          <div className="register-input file-input-div">
+            <label className="label" htmlFor="file">Choose profile picture {image ? <FaCheck /> : null}</label>
+            <input type="file" accept="image/*" onChange={this.onImageChange} id="file" />
+          </div>
+          <div className="register-input">
+            <label htmlFor="name">Name:</label>
+            <input type="text" name='name' value={name} onChange={this.onInputChange} placeholder="Enter username" id="name" />
+          </div>
+          <div className="register-input">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name='email' value={email} onChange={this.onInputChange} placeholder="Enter e-mail" id="email" />
+          </div>
+          <div className="register-input">
+            <label htmlFor="password">Password:</label>
+            <input type="password" name='password' value={password} onChange={this.onInputChange} placeholder="Enter password" id="password" />
+          </div>
+          {
+            Object.keys(validationErrors).length ? (
+              <div className="error-alert">
+                <span>{validationErrors.email}</span>
+                <span>{validationErrors.password}</span>
+                <span>{validationErrors.name}</span>
+              </div>
+            ) : null
+          }
+          <button onClick={this.handleRegister} className="btn">Sign up</button>
+        </div>
+      </div>
+    )
+  }
 }
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,21 +1,29 @@
 import { Component } from "react";
 import './user-page.css'
 
-export default class UserPage extends Component{
+export default class UserPage extends Component {
 
-    render(){
-        const {name, email, password, image}=this.props.userData
-        return (
-            <div className="wrapper-userpage">
-                <ul>
-                    <li>Name: {name}</li>
-                    <li>E-mail: {email}</li>
-                    <li>Password: {password}</li>
-                </ul>
-                {
-                    image ? <img src={image} alt="User's" className="img"/> : null
-                }
-            </div>
-        )
-    }
+  render() {
+    const { name, email, password, image } = this.props.userData
+    return (
+      <div className="wrapper-userpage">
+        <h1>Welcome, <span>{name ? name : "Guest"}</span></h1>
+        <h4>Your personal info</h4>
+        {
+          image ? 
+            <div className="image-div">Profile Picture:<img src={image} alt="User's" className="img" />
+              </div> : null
+        }
+        <div>
+          Name: <span>{name}</span>
+        </div>
+        <div>
+          E-mail: <span>{email}</span>
+        </div>
+        <div>
+          Password: <span>{password}</span>
+        </div>
+      </div>
+    )
+  }
 }
